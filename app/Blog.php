a0cf64f2349d7b434c09c764331173ca367bfb99
techrifat <kmrifat@techcoderz.com>
2020-03-22 15:35:44 +0600

    added fallback language on model

--- app/Blog.php
+++ app/Blog.php
@@ -9,17 +9,18 @@ class Blog extends Model
     public function getLang()
     {
         $blogTranslate = $this->hasOne(BlogTranslation::class)->where('lang', config('app.locale'));
-        if (empty($blogTranslate->id)) {
-            $blogTranslate = $this->hasOne(BlogTranslation::class);
+        if (!$blogTranslate->exists()) {
+            $blogTranslate = $this->hasOne(BlogTranslation::class)->where('lang', config('app.fallback_locale'));
         }
+
         return $blogTranslate;
     }
 
     public function getLangByUrl()
     {
         $blogTranslate = $this->hasOne(BlogTranslation::class)->where('lang', request()->query('lang'));
-        if (empty($blogTranslate)) {
-            $blogTranslate = $this->hasOne(BlogTranslation::class);
+        if (!$blogTranslate->exists()) {
+            $blogTranslate = $this->hasOne(BlogTranslation::class)->where('lang', config('app.fallback_locale'));
         }
         return $blogTranslate;
     }
