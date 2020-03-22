7c7b767ab49b7809b8f3c16c5b16bc424298a0c7
techrifat <kmrifat@techcoderz.com>
2020-03-22 15:24:05 +0600

    bug fix on fallback lang

--- app/Blog.php
+++ app/Blog.php
@@ -9,8 +9,8 @@ class Blog extends Model
     public function getLang()
     {
         $blogTranslate = $this->hasOne(BlogTranslation::class)->where('lang', config('app.locale'));
-        if (!is_object($blogTranslate)) {
-            $this->hasOne(BlogTranslation::class);
+        if (empty($blogTranslate->id)) {
+            $blogTranslate = $this->hasOne(BlogTranslation::class);
         }
         return $blogTranslate;
     }
@@ -18,8 +18,8 @@ class Blog extends Model
     public function getLangByUrl()
     {
         $blogTranslate = $this->hasOne(BlogTranslation::class)->where('lang', request()->query('lang'));
-        if (!is_object($blogTranslate)) {
-            $this->hasOne(BlogTranslation::class);
+        if (empty($blogTranslate)) {
+            $blogTranslate = $this->hasOne(BlogTranslation::class);
         }
         return $blogTranslate;
     }
